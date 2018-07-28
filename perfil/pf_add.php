d2482c217f18d84c42a40d5303cb969c1ac9ddda
Lorelei Gabriele <loreleigab@hotmail.com>
2018-07-28 01:26:47 -0300

    Início da tela de cadastro de PF

---
+++ perfil/pf_add.php
@@ -0,0 +1,105 @@
+<section id="list_items" class="home-section bg-white">
+	<div class="container"><?php include 'includes/menu.php'; ?>
+		<p align="left"><strong><?php echo saudacao(); ?>, <?php echo $_SESSION['nome']; ?>!</strong></p>
+		<div class="form-group">
+			<h5>Cadastro de Pessoa Física</h5>
+		</div>
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10">
+				<h5><?php if(isset($mensagem)){echo $mensagem;};?></h5>
+				<form class="form-horizontal" role="form" action="?perfil=artista_pj_cadastro" method="post">
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8"><strong>Nome: *</strong><br/>
+							<input type="text" class="form-control" name="nome" placeholder="Nome completo" maxlength="70" required>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-6"><strong>RG: *</strong><br/>
+							<input type="text" class="form-control" name="rg" maxlength="20">
+						</div>
+						<div class="col-md-6"><strong>CPF: *</strong><br/>
+							<input type="text" class="form-control" name="cpf">
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-6"><strong>Telefone #1 *:</strong><br/>
+							<input type="text" class="form-control" name="telefone1" id="telefone" onkeyup="mascara( this, mtel );" maxlength="15" placeholder="Exemplo: (11) 98765-4321">
+						</div>
+						<div class="col-md-6"><strong>Telefone #2:</strong><br/>
+							<input type="text" class="form-control" name="telefone2" id="telefone" onkeyup="mascara( this, mtel );" maxlength="15" placeholder="Exemplo: (11) 98765-4321">
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8"><strong>E-mail *:</strong><br/>
+							<input type="text" class="form-control" name="email" placeholder="E-mail" maxlength="60" >
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-6"><strong>CEP *:</strong><br/>
+							<input type="text" class="form-control" id="CEP" name="CEP" placeholder="CEP">
+						</div>
+						<div class="col-md-6" align="left"><i>Clique no número do CEP e pressione a tecla Tab para carregar</i>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8"><strong>Endereço:</strong><br/>
+							<input type="text" readonly class="form-control" id="Endereco" name="Endereco" placeholder="Endereço">
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-6"><strong>Número *:</strong><br/>
+							<input type="text" class="form-control" id="Numero" name="Numero" placeholder="Numero" maxlength="5" required>
+						</div>
+						<div class=" col-md-6"><strong>Complemento:</strong><br/>
+							<input type="text" class="form-control" id="Complemento" name="Complemento" placeholder="Complemento" maxlength="20">
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8"><strong>Bairro:</strong><br/>
+							<input type="text" readonly class="form-control" id="Bairro" name="Bairro" placeholder="Bairro">
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-6"><strong>Cidade:</strong><br/>
+							<input type="text" readonly class="form-control" id="Cidade" name="Cidade" placeholder="Cidade">
+						</div>
+						<div class="col-md-2"><strong>Estado:</strong><br/>
+							<input type="text" readonly class="form-control" id="Estado" name="Estado" placeholder="Estado">
+						</div>
+						<div class="col-md-2"><strong>Valor Ponto:</strong><br/>
+							<input type="text" class="form-control" name="ponto" maxlength="15">
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8"><strong>Forma de Pagamento:</strong><br/>
+							<input type="text" class="form-control" name="drt"  maxlength="15">
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8"><strong>Observação:</strong><br/>
+							<textarea></textarea>
+						</div>
+					</div>
+
+					<!-- Botão para Gravar -->
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8">
+							<input type="submit" value="GRAVAR" class="btn btn-theme btn-lg btn-block">
+						</div>
+					</div>
+				</form>
+			</div>				
+		</div>
+	</div>
+</section>
