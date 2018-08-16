cdfd4148998bcd49d067335fe7b988b6846804a2
LoreleiGab <loreleigab@hotmail.com>
2018-08-16 09:28:26 -0300

    Ajuste no menu

--- perfil/includes/menu.php
+++ perfil/includes/menu.php
@@ -1,6 +1,3 @@
-<?php
-$pasta = "?perfil=";
-?>
 <div class="menu-area">
 	<div id="dl-menu" class="dl-menuwrapper">
 		<button class="dl-trigger">Open Menu</button>
@@ -8,30 +5,30 @@ $pasta = "?perfil=";
 			<li><a href="<?php echo $pasta ?>index">Home</a></li>
 			<li><a href="#">Clientes</a>
 				<ul class="dl-submenu">
-					<li><a href="<?php echo $pasta ?>pf_add">Cadastra PF</a></li>
-					<li><a href="<?php echo $pasta ?>pj_add">Cadastra PJ</a></li>
-					<li><a href="<?php echo $pasta ?>pf_list">Lista PF</a></li>
-					<li><a href="<?php echo $pasta ?>pj_list">Lista PJ</a></li>
+					<li><a href="?perfil=pf_add">Cadastra PF</a></li>
+					<li><a href="?perfil=pj_add">Cadastra PJ</a></li>
+					<li><a href="?perfil=pf_list">Lista PF</a></li>
+					<li><a href="?perfil=pj_list">Lista PJ</a></li>
 				</ul>
 			</li>
 			<li><a href="#">Condutor</a>
 				<ul class="dl-submenu">
-					<li><a href="<?php echo $pasta ?>condutor_add">Cadastra Condutor</a></li>
-					<li><a href="<?php echo $pasta ?>condutor_list">Lista</a></li>
-					<li><a href="<?php echo $pasta ?>adiantamento_add">Cadastra Adiantamento</a></li>
-					<li><a href="<?php echo $pasta ?>adiantamento_list">Lista Adiantamento</a></li>
+					<li><a href="?perfil=condutor_add">Cadastra Condutor</a></li>
+					<li><a href="?perfil=condutor_list">Lista</a></li>
+					<li><a href="?perfil=adiantamento_add">Cadastra Adiantamento</a></li>
+					<li><a href="?perfil=adiantamento_list">Lista Adiantamento</a></li>
 				</ul>
 			</li>
 			<li><a href="#">O.S.</a>
 				<ul class="dl-submenu">
-					<li><a href="<?php echo $pasta ?>os_add_cliente">Cadastra</a></li>
-					<li><a href="<?php echo $pasta ?>os_pesquisa">Pesquisa</a></li>
+					<li><a href="?perfil=os_add_cliente">Cadastra</a></li>
+					<li><a href="?perfil=os_pesquisa">Pesquisa</a></li>
 				</ul>
 			</li>
 			<li><a href="#">Relatórios</a>
 				<ul class="dl-submenu">
-					<li><a href="<?php echo $pasta ?>condutor_pesquisa">Por Condutor</a></li>
-					<li><a href="../pdf/excel_cliente.php">Por Cliente</a></li>
+					<li><a href="?perfil=condutor_pesquisa">Por Condutor</a></li>
+					<li><a href="?perfil=cliente_pesquisa">Por Cliente</a></li>
 				</ul>
 			</li>
 			<li style="color:white;">-------------------------</li>
