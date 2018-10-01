dbe965c5d47e018a244b10b08b05e8ac9e32dec7
Lorelei Gabriele <loreleigab@hotmail.com>
2018-09-30 21:42:57 -0300

    Mudança do texto de abertura

--- perfil/index.php
+++ perfil/index.php
@@ -2,7 +2,9 @@
 	<div class="container"><?php include 'includes/menu.php'; ?>
 		<p align="left"><strong><?php echo saudacao(); ?>, <?php echo $_SESSION['nome']; ?>!</strong></p>
 		<div class="form-group">
-			<h5>teste</h5>
+			<h5>L&J Transportes</h5>
+            <p align="justify">L&J Transportes atua no ramo de transporte há 1 (um) ano com experiência de 5 anos no mercado de trabalho, fundada pelo Lucas Carloto onde trabalhou com diversas formas de entrega após estudo e análise do mercado no segmento de transportes rápidos, a L&J foi desenvolvida com intuito de servir seus clientes, constatamos que a segurança, agilidade e qualidade são os nossos principais fatores, tornando-se exigências primordiais.</p>
+            <p align="justify">Baseando-se nestes princípios a empresa L&J TRANSPORTES foi criada, para proporcionar aos nossos clientes o que é fundamental no ramo. SEGURANÇA, AGILIDADE E QUALIDADE.</p>
 		</div>
 		<div class="row">
 			<div class="col-md-offset-1 col-md-10">
