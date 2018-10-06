5e6ff8831c1416946714e36c8227bc823e5ed608
Lorelei Gabriele <loreleigab@hotmail.com>
2018-10-06 15:21:59 -0300

    Botão de sair do sistema no ambiente do condutor

--- funcionario/index.php
+++ funcionario/index.php
@@ -1,7 +1,10 @@
 <section id="list_items" class="home-section bg-white">
 	<div class="container">
 		<p align="left"><strong><?php echo saudacao(); ?>, <?php echo $_SESSION['nome']; ?>!</strong></p>
-		<div class="form-group">
+        <div class="form-group" align="right">
+            <a href="../include/logoff.php"><button class="btn btn-theme btn-sm" style='border-radius: 7px;'><strong>Sair</strong></button></a>
+        </div>
+        <div class="form-group">
 			<h5>L&J Transportes</h5>
             <p align="justify">L&J Transportes atua no ramo de transporte há 1 (um) ano com experiência de 5 anos no mercado de trabalho, fundada pelo Lucas Carloto onde trabalhou com diversas formas de entrega após estudo e análise do mercado no segmento de transportes rápidos, a L&J foi desenvolvida com intuito de servir seus clientes, constatamos que a segurança, agilidade e qualidade são os nossos principais fatores, tornando-se exigências primordiais.</p>
             <p align="justify">Baseando-se nestes princípios a empresa L&J TRANSPORTES foi criada, para proporcionar aos nossos clientes o que é fundamental no ramo. SEGURANÇA, AGILIDADE E QUALIDADE.</p>
