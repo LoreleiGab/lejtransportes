486f018b12109491e1498abf1832f37580cb0573
Juzenhu <Juzenhu@gmail.com>
2018-11-11 11:25:49 -0200

    Introdução , alteração do histórico

--- cliente/index.php
+++ cliente/index.php
@@ -6,8 +6,9 @@
         </div>
         <div class="form-group">
 			<h5>L&J Transportes</h5>
-            <p align="justify">L&J Transportes atua no ramo de transporte desde de 2016 com experiência de 10 anos no mercado de trabalho, fundada pelo Lucas Ramalho onde trabalhou com diversas formas de entrega após estudo e análise do mercado no segmento de transportes rápidos junto com o seu primo Ivamar Ramalho a L&J foi fundada e desenvolvida com intuito de servir seus clientes, constatamos que a segurança, agilidade e qualidade são os nossos principais fatores, tornando-se exigências primordiais em nosso ramo.</p>
+            <p align="justify">L&J Transportes atua no ramo de transporte desde de 2016 com experiência de 10 anos no mercado de trabalho, fundada pelo Lucas Ramalho onde trabalhou com diversas formas de entrega após estudos e análise do mercado no segmento de transportes rápidos junto com o seu primo Ivamar Ramalho a L&J foi fundada e desenvolvida com intuito de servir seus clientes, constatamos que a segurança, agilidade e qualidade são os nossos principais fatores, tornando-se exigências primordiais em nosso ramo.</p>
             <p align="justify">Baseando-se nestes princípios a empresa L&J TRANSPORTES foi criada, para proporcionar aos nossos clientes o que é fundamental no ramo. SEGURANÇA, AGILIDADE E QUALIDADE.</p>
+            <p align="justify">No 2º Semestre de 2018 conforme as demandas e solicitações de nossos clientes acrescentamos os transportes de medios e grandes volumes e também com veículo específico em refrigeração - 10º C, com isso temos veículos adequados, monitorados e aprimoramento em gestão de risco.</p>
             <p align="center"><b><?php echo $_SESSION['nome']; ?></b> Agradecemos a sua cooperação e estamos a sua disposição!</p>
 		</div>
         <div class="well">
--- supervisor/index.php
+++ supervisor/index.php
@@ -3,8 +3,9 @@
 		<p align="left"><strong><?php echo saudacao(); ?>, <?php echo $_SESSION['nome']; ?>!</strong></p>
 		<div class="form-group">
 			<h5>L&J Transportes</h5>
-            <p align="justify">L&J Transportes atua no ramo de transporte desde de 2016 com experiência de 10 anos no mercado de trabalho, fundada pelo Lucas Ramalho onde trabalhou com diversas formas de entrega após estudo e análise do mercado no segmento de transportes rápidos junto com o seu primo Ivamar Ramalho a L&J foi fundada e desenvolvida com intuito de servir seus clientes, constatamos que a segurança, agilidade e qualidade são os nossos principais fatores, tornando-se exigências primordiais em nosso ramo.</p>
+            <p align="justify">L&J Transportes atua no ramo de transporte desde de 2016 com experiência de 10 anos no mercado de trabalho, fundada pelo Lucas Ramalho onde trabalhou com diversas formas de entrega após estudos e análise do mercado no segmento de transportes rápidos junto com o seu primo Ivamar Ramalho a L&J foi fundada e desenvolvida com intuito de servir seus clientes, constatamos que a segurança, agilidade e qualidade são os nossos principais fatores, tornando-se exigências primordiais em nosso ramo.</p>
             <p align="justify">Baseando-se nestes princípios a empresa L&J TRANSPORTES foi criada, para proporcionar aos nossos clientes o que é fundamental no ramo. SEGURANÇA, AGILIDADE E QUALIDADE.</p>
+			<p align="justify">No 2º Semestre de 2018 conforme as demandas e solicitações de nossos clientes acrescentamos os transportes de medios e grandes volumes e também com veículo específico em refrigeração - 10º C, com isso temos veículos adequados, monitorados e aprimoramento em gestão de risco.</p>
 		</div>
 		<div class="row">
 			<div class="col-md-offset-1 col-md-10">
